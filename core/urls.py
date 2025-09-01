004eabc2576abee30387d3cbee3af6aa79a0c6af
MehrojOltinboyev <mehrojoltinboyev25@gmail.com>
2025-09-01 13:39:57 +0300

    initial commit

---
+++ core/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('ckeditor', include('ckeditor_uploader.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),  # signup, custom viewlar
+    path('accounts/', include('django.contrib.auth.urls')),  # login, logout, password_change, va h.k.
+    path('articles/',include('articles.urls')),
+    path('', include('pages.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
